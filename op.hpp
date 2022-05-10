9228463219e2db7d50857b065fa413e3e602c8d2
acast338 <acast338@ucr.edu>
2022-05-10 12:50:51 -0700

    add unit test for first mock in Op

--- op.hpp
+++ op.hpp
@@ -10,4 +10,12 @@ class Op : public Base {
         virtual std::string stringify() { return ""; }
 };
 
+class SevenOpMock: public Base {
+    public:
+        SevenOpMock() { }
+
+        virtual double evaluate() { return 7.5; }
+        virtual std::string stringify() { return "7.5"; }
+};
+
 #endif //__OP_HPP__
--- op_test.hpp
+++ op_test.hpp
@@ -10,4 +10,9 @@ TEST(OpTest, OpEvaluateNonZero) {
     EXPECT_EQ(test->evaluate(), 8);
 }
 
+TEST(OpMockTest, OpEvaluateSeven) {
+    SevenOpMock* test = new SevenOpMock();
+    EXPECT_EQ(test->evaluate(), 7.5);
+}
+
 #endif //__OP_TEST_HPP__
